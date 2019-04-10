a82a7f9c33d33a43a33854b0d0300cc3effc14f4
Jonas Kello <jonaskello@users.noreply.github.com>
2019-04-10 10:51:32 +0200

    Add update entity (#11)
    * Add update entity
    
    * Add test

--- test/test-data.ts
+++ test/test-data.ts
@@ -1,4 +1,5 @@
 import { createEntityTestData } from "./test-data/create-entity";
+import { updateEntityTestData } from "./test-data/update-entity";
 import { deleteEntityTestData } from "./test-data/delete-entity";
 import { updateFieldTestData } from "./test-data/update-field";
 import { insertElementTestData } from "./test-data/insert-element";
@@ -11,6 +12,7 @@ import { OneTest } from "./test-data/one-test";
 
 export const testData: ReadonlyArray<OneTest> = [
   ...createEntityTestData,
+  ...updateEntityTestData,
   ...deleteEntityTestData,
   ...updateFieldTestData,
   ...insertElementTestData,
---
+++ test/test-data/update-entity.ts
@@ -0,0 +1,24 @@
+import { updateEntity } from "../../src";
+import { OneTest } from "./one-test";
+
+export const updateEntityTestData: ReadonlyArray<OneTest> = [
+  {
+    name:
+      "updateEntity should not do anything if entity does not exist in cache",
+    patches: [updateEntity("obj1", { name: "foo", bar: "zoo" })],
+    cacheBefore: {},
+    cacheAfter: {}
+  },
+  {
+    name: "updateEntity should update if entity exist in cache",
+    patches: [updateEntity("obj1", { name: "foo", bar: "zoo" })],
+    cacheBefore: { obj1: { id: "obj1", name: "name", bar: "bar" } },
+    cacheAfter: { obj1: { id: "obj1", name: "foo", bar: "zoo" } }
+  },
+  {
+    name: "updateEntity should create fields that does not exist in cache",
+    patches: [updateEntity("obj1", { name: "foo", bar: "zoo" })],
+    cacheBefore: { obj1: { id: "obj1", name: "name" } },
+    cacheAfter: { obj1: { id: "obj1", name: "foo", bar: "zoo" } }
+  }
+];

82ef5c3028a047f74c73cdc84adfbb4e667a3413
Jonas Kello <jonas.kello@gmail.com>
2019-09-28 17:51:22 +0200

    Cleanup

--- test/apply.test.ts
+++ test/apply.test.ts
@@ -1,24 +1,5 @@
-// import * as test from "tape";
 import { apply } from "../src";
 import { testData } from "./test-data";
-
-// test("apply", t => {
-//   const tests = testData.some(t3 => !!t3.only)
-//     ? testData.filter(t2 => t2.only)
-//     : testData;
-//   tests.forEach(testCase => {
-//     t.test(testCase.name, st => {
-//       const [actualCache, actualStale] = apply(
-//         testCase.patches,
-//         testCase.cacheBefore,
-//         testCase.staleBefore || {}
-//       );
-//       st.deepEqual(actualCache, testCase.cacheAfter);
-//       st.deepEqual(actualStale, testCase.staleAfter || {});
-//       st.end();
-//     });
-//   });
-// });
 
 describe("apply", () => {
   testData.forEach(testCase => {
